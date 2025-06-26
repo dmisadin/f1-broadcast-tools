640682731c78f006398361580f93d35f275a54cc
dmisadin <dominik.misadin@gmail.com>
2025-06-26 02:15:14 +0200

    Add green flag to session-details

--- F1BroadcastTools.Client/src/app/features/timing-tower/session-details/session-details.component.ts
+++ F1BroadcastTools.Client/src/app/features/timing-tower/session-details/session-details.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from "@angular/core";
-import { SafetyCarStatus } from "../../../shared/models/Enumerations";
+import { Component, Input, computed, effect, input, signal } from '@angular/core';
+import { SafetyCarStatus } from '../../../shared/models/Enumerations';
 
 @Component({
     standalone: false,
@@ -11,8 +11,30 @@ export class SessionDetailsComponent {
     @Input() currentLap: number;
     @Input() totalLaps: number;
     @Input() safetyCarStatus: SafetyCarStatus;
-    @Input() sectorYellowFlags: boolean[];
+    sectorYellowFlags = input<boolean[]>([]);
+    greenFlagVisible = signal(false);
+    private previousHadYellow = signal(false);
+    hasAnyYellow = computed(() => this.sectorYellowFlags().some(f => f));
+
+    private timeoutHandle: any;
+
+    constructor() {
+        effect(() => {
+            const current = this.hasAnyYellow();
+            const previous = this.previousHadYellow();
+
+            // Detect transition: yellow → green
+            if (previous && !current) {
+                this.greenFlagVisible.set(true);
+                clearTimeout(this.timeoutHandle);
+                this.timeoutHandle = setTimeout(() => {
+                    this.greenFlagVisible.set(false);
+                }, 4000);
+            }
+
+            this.previousHadYellow.set(current);
+        });
+    }
 
     SafetyCarStatus = SafetyCarStatus;
-
 }
