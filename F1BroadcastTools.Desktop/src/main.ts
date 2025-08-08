52c812e344cee3c41c17683e70098fb0581961e2
dmisadin <dominik.misadin@gmail.com>
2025-08-08 14:42:40 +0200

    Add ElectronJS directory

---
+++ F1BroadcastTools.Desktop/src/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import { spawn } from 'child_process';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let backendProcess: any;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            contextIsolation: true,
+        }
+    });
+
+    win.loadFile(path.join(__dirname, '../../F1BroadcastTools.Client/dist/f1-broadcast-tools/browser/index.html'));
+}
+
+app.whenReady().then(() => {
+    const backendPath = path.join(__dirname, '../../F1GameDataParser/bin/Release/net8.0/win-x64/publish/F1GameDataParser.exe');
+    backendProcess = spawn(backendPath, [], {
+        detached: true,
+        stdio: 'ignore',
+    });
+    backendProcess.unref();
+
+    createWindow();
+});
