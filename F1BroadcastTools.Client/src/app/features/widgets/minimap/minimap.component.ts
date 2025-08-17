28361c09a58d138c5c9c28cc8952a2df8816e470
dmisadin <dominik.misadin@gmail.com>
2025-08-17 19:54:20 +0200

    Fragment minimap signals

--- F1BroadcastTools.Client/src/app/features/widgets/minimap/minimap.component.ts
+++ F1BroadcastTools.Client/src/app/features/widgets/minimap/minimap.component.ts
@@ -1,36 +1,47 @@
 import { Component, OnDestroy, OnInit, signal } from "@angular/core";
 import { WebSocketService } from "../../../core/services/websocket.service";
-import { Minimap } from "../../../shared/models/Minimap";
+import { Minimap, MinimapCar } from "../../../shared/models/Minimap";
 import { WidgetBaseComponent } from "../widget-base.component";
 import { CommonModule } from "@angular/common";
+import { MinimapCarComponent } from "./minimap-car/minimap-car.component";
 
 @Component({
     selector: 'minimap',
     templateUrl: 'minimap.component.html',
     styleUrl: 'minimap.component.css',
-    imports: [CommonModule],
+    imports: [CommonModule, MinimapCarComponent],
     providers: [WebSocketService]
 })
 export class MinimapComponent extends WidgetBaseComponent<Minimap> implements OnInit, OnDestroy {
-    minimap = signal<Minimap | null>(null);
+    trackId = signal<number | null>(null);
+    //spectatorCarIdx = signal<number | null>(null);
+    rotation = signal<number | null>(null);
+    cars = signal<MinimapCar[] | null>(null);
 
     constructor(private webSocketService: WebSocketService<Minimap>) { super(); }
 
     ngOnInit(): void {
         const placeholder = this.placeholderData();
         if (placeholder) {
-            this.minimap.set(placeholder);
+            this.updateState(placeholder);
             return;
         }
 
         this.webSocketService.connect('ws://localhost:5000/ws/minimap');
 
         this.webSocketService.onMessage().subscribe((data: Minimap) => {
-            this.minimap.set(data);
+            this.updateState(data);
         });
     }
 
     ngOnDestroy(): void {
         this.webSocketService.disconnect();
+    }
+
+    private updateState(data: Minimap) {
+        this.trackId.set(data.trackId)
+        //this.spectatorCarIdx.set(data.spectatorCarIdx)
+        this.rotation.set(data.rotation ?? 0);
+        this.cars.set(data.cars);
     }
 }
--- F1BroadcastTools.Client/src/app/features/widgets/widget-base.component.ts
+++ F1BroadcastTools.Client/src/app/features/widgets/widget-base.component.ts
@@ -3,4 +3,6 @@ import { signal, inject, Component, Injectable, input } from "@angular/core";
 @Component({ template: '' })
 export abstract class WidgetBaseComponent<TViewModel> {
     placeholderData = input<TViewModel>();
+
+    protected abstract setState(data: TViewModel): void;
 }
