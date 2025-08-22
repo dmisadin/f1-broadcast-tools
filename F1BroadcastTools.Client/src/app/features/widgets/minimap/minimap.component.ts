fbeb25e42b3c8c7f7d6449d063750b3164e7373a
dmisadin <dominik.misadin@gmail.com>
2025-08-22 15:27:18 +0200

    Remove small component due to no impact with fine-grained signals

--- F1BroadcastTools.Client/src/app/features/widgets/minimap/minimap.component.ts
+++ F1BroadcastTools.Client/src/app/features/widgets/minimap/minimap.component.ts
@@ -3,20 +3,19 @@ import { WebSocketService } from "../../../core/services/websocket.service";
 import { Minimap, MinimapCar } from "../../../shared/models/Minimap";
 import { WidgetBaseComponent } from "../widget-base.component";
 import { CommonModule } from "@angular/common";
-import { MinimapCarComponent } from "./minimap-car/minimap-car.component";
 
 @Component({
     selector: 'minimap',
     templateUrl: 'minimap.component.html',
     styleUrl: 'minimap.component.css',
-    imports: [CommonModule, MinimapCarComponent],
+    imports: [CommonModule],
     providers: [WebSocketService],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MinimapComponent extends WidgetBaseComponent<Minimap> implements OnInit, OnDestroy {
     trackId = signal<number | null>(null);
     //spectatorCarIdx = signal<number | null>(null);
-    rotation = signal<number | null>(null);
+    //rotation = signal<number | null>(null);
     cars = signal<MinimapCar[] | null>(null);
 
     trackUrl = computed(() => {
@@ -49,7 +48,7 @@ export class MinimapComponent extends WidgetBaseComponent<Minimap> implements On
     protected override setState(data: Minimap): void {
         this.trackId.set(data.trackId)
         //this.spectatorCarIdx.set(data.spectatorCarIdx)
-        this.rotation.set(data.rotation ?? 0);
+        //this.rotation.set(data.rotation ?? 0);
         this.cars.set(data.cars);
     }
 }
