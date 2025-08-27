dbdf301f723b38955bf3efcb78cbb072edd06401
dmisadin <dominik.misadin@gmail.com>
2025-08-28 00:42:09 +0200

    Add new navigation

--- F1BroadcastTools.Client/src/app/features/home/home.component.ts
+++ F1BroadcastTools.Client/src/app/features/home/home.component.ts
@@ -1,12 +1,21 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'home',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+    constructor(private router: Router, private location: Location) { }
 
+    back() {
+        this.location.back();
+    }
+
+    forward() {
+        this.location.forward();
+    }
 }
