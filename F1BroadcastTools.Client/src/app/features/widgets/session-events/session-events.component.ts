5b226c02302af1f5045475e617a5dc88fd839af6
dmisadin <dominik.misadin@gmail.com>
2025-10-20 14:52:43 +0200

    Add blank session-events component

---
+++ F1BroadcastTools.Client/src/app/features/widgets/session-events/session-events.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'session-events',
+	imports: [],
+	templateUrl: './session-events.component.html',
+	styleUrl: './session-events.component.css'
+})
+export class SessionEventsComponent {
+
+}
---
+++ F1BroadcastTools.Client/src/app/shared/models/session-event.model.ts
@@ -0,0 +1,6 @@
+export interface SessionEvent {
+    vehicleIdx: number | null;
+    involvedVehicleIdx: number | null;
+    title: string;
+    description: string;
+}
